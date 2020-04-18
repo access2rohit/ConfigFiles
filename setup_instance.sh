88880444bb22389f8ad5c8c09a3b0c5fd8c72ef1
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2020-04-17 19:17:11 -0700

    [Update] pull mxnet code

--- setup_instance.sh
+++ setup_instance.sh
@@ -2,6 +2,10 @@
 sudo apt update
 sudo apt install -y build-essential ninja-build cmake ccache htop stow zsh zlib1g-dev libffi-dev libssl-dev libbz2-dev libsqlite3-dev libreadline6-dev libjemalloc-dev libopenblas-dev libopencv-dev
 
+mkdir ${HOME}/workspace
+cd ${HOME}/workspace
+git clone --recursive https://github.com/apache/incubator-mxnet.git
+
 mkdir ${HOME}/temp
 cd ${HOME}/temp
 
@@ -9,10 +13,15 @@ wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/bashrc
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/gitconfig
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/screenrc
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_ocaml_unison.sh
+wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/mxnet_config
+
 
 cat bashrc >> ${HOME}/.bashrc
 cat gitconfig >> ${HOME}/.gitconfig
 cat screenrc >> ${HOME}/.screenrc
+cat mxnet_config >> ${HOME}/workspace/incubator-mxnet/.git/config
+
+cd ${HOME}
 
 bash install_ocaml_unison.sh
 
