5d42a40cbf3945518a0562ea273d1fa79e80359f
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2020-04-17 19:01:13 -0700

    Update setup_instance.sh

--- setup_instance.sh
+++ setup_instance.sh
@@ -3,12 +3,17 @@ sudo apt update
 sudo apt install -y build-essential ninja-build cmake ccache htop stow zsh zlib1g-dev libffi-dev libssl-dev libbz2-dev libsqlite3-dev libreadline6-dev libjemalloc-dev libopenblas-dev libopencv-dev
 
 mkdir ${HOME}/temp
-cd ${home}/temp
+cd ${HOME}/temp
 
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/bashrc
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/gitconfig
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/screenrc
+wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_ocaml_unison.sh
 
 cat bashrc >> ${HOME}/.bashrc
 cat gitconfig >> ${HOME}/.gitconfig
 cat screenrc >> ${HOME}/.screenrc
+
+bash install_ocaml_unison.sh
+
+cd ${HOME}
