2213c69681d242cf89950505c95da6ffc6eb9f24
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2021-03-24 16:42:34 -0700

    Update install_python_debug.sh

--- install_python_debug.sh
+++ install_python_debug.sh
@@ -7,6 +7,6 @@ sudo make OPT=-g && sudo make install
 
 ## Add python valgrind suppression file
 cp $DIR/Python-3.6.12/Misc/valgrind-python.supp $HOME/workspace/incubator-mxnet
-echo "export PATH=/usr/bin:/bin:/opt/debugpython/bin:$PATH" >> ${HOME}/.bashrc
+echo "export PATH=/usr/bin:/bin:/opt/debugpython/bin:\$PATH" >> ${HOME}/.bashrc
 sudo update-alternatives --install /usr/bin/python python /opt/debugpython/bin/python3 10
 echo "Uncomment PyObject_Free and PyObject_Realloc in the valgring suppression file."
