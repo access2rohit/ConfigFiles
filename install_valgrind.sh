1375c238f72867479887265557890fb8a0d9a1b3
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2021-03-24 16:58:03 -0700

    fix paths in ENV vars

--- install_valgrind.sh
+++ install_valgrind.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 DIR=$(pwd)
+LOC=/usr/local/valgrind
 git clone git://sourceware.org/git/valgrind.git
 cd $DIR/valgrind
 ./autogen.sh
-./configure --prefix=/usr/local/valgrind
+./configure --prefix=${LOC}
 make
 sudo make install
-echo "export PATH=\$PATH:${DIR}/valgrind/bin" >> ${HOME}/.bashrc
-echo "export VALGRIND_LIB=${DIR}/valgrind/lib/valgrind" >> ${HOME}/.bashrc
+echo "export PATH=\$PATH:${LOC}/bin" >> ${HOME}/.bashrc
+echo "export VALGRIND_LIB=${LOC}/lib/valgrind" >> ${HOME}/.bashrc
