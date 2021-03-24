c8b4b2eaac5612833b3c932157d4ee348aaf2aeb
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2021-03-24 13:25:05 -0700

    added missing download commands to fetch installation scripts for debug tools

--- setup_instance.sh
+++ setup_instance.sh
@@ -32,6 +32,8 @@ wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/vimrc
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_vundle.sh
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_ocaml_unison.sh
 wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_cmake.sh
+wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_python_debug.sh
+wget https://raw.githubusercontent.com/access2rohit/ConfigFiles/master/install_valgrind.sh
 
 cat bashrc >> ${HOME}/.bashrc
 cat gitconfig >> ${HOME}/.gitconfig
@@ -45,11 +47,9 @@ bash install_cmake.sh
 cd ${HOME}
 rm -rf ${HOME}/temp
 
-# install debug tools if instance type is "Debug"
+# Install debug tools if instance setup mode is "Debug"
 typeset -l $1
 if [[ $1 == "debug" ]]; then
-  mkdir -p ${HOME}/debugging_tools
-  cd ${HOME}/debugging_tools
   bash install_python_debug.sh
   bash install_valgrind.sh
 fi
