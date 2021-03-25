6e68506eeb62cc0b61e2339d8b73426ae2f83c19
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2021-03-25 15:08:41 -0700

    Update install_python_debug.sh

--- install_python_debug.sh
+++ install_python_debug.sh
@@ -1,13 +1,14 @@
 DIR=$(pwd)
-wget https://www.python.org/ftp/python/3.7.10/Python-3.7.10.tgz
-tar -xvzf Python-3.7.10.tgz
-cd Python-3.7.10
+VERSION=3.7.10
+wget https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
+tar -xvzf Python-$VERSION.tgz
+cd Python-$VERSION
 ./configure --with-pydebug --without-pymalloc --with-valgrind --prefix /opt/debugpython/
 sudo make OPT=-g && sudo make install
 
 ## Add python valgrind suppression file
-cp $DIR/Python-3.7.10/Misc/valgrind-python.supp $HOME/workspace/incubator-mxnet
+cp $DIR/Python-$VERSION/Misc/valgrind-python.supp $HOME/workspace/incubator-mxnet
 echo "export PATH=/usr/bin:/bin:/opt/debugpython/bin:\$PATH" >> ${HOME}/.bashrc
 sudo update-alternatives --install /usr/bin/python python /opt/debugpython/bin/python3 10
-rm Python-3.7.10.tgz
+rm Python-$VERSION.tgz
 echo "Uncomment PyObject_Free and PyObject_Realloc in the valgring suppression file."
