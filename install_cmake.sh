2793b656dd519d3deacb71f3c41e14411b2eed10
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2023-01-05 00:19:01 -0800

    Update cmake version to 3.20.2

--- install_cmake.sh
+++ install_cmake.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+export CMAKE_VERSION=3.20.2
 sudo apt-get remove cmake -y
-
-wget http://www.cmake.org/files/v3.13/cmake-3.13.3.tar.gz
-tar xf cmake-3.13.3.tar.gz
-cd cmake-3.13.3
-./configure
-make
-sudo make install
+sudo apt-get install build-essential libssl-dev
+wget -q https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz
+tar -zxvf cmake-${CMAKE_VERSION}.tar.gz
+cd cmake-${CMAKE_VERSION}
+ ./bootstrap
+make -j $(nproc) && make install
