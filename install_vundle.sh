6a7bc1c33773f8930d66a21c8cee1cd54395dd62
Rohit Kumar Srivastava <srivastava.141@osu.edu>
2023-01-05 01:45:54 -0800

    Update install_vundle.sh

--- install_vundle.sh
+++ install_vundle.sh
@@ -3,3 +3,7 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Install plugins present in vimrc
 vim +PluginInstall +qall
+
+# Compile YCM
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
